485d1a3565e097dfae4985caa561ec4b6baf07b3
ifwhy <ifanugrh02@student.uns.ac.id>
2023-09-04 18:21:04 +0700

    Menambahkan suku ke-n dalam barisan fibonacci

--- latihan.cpp
+++ latihan.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
+int fibonacci(int n){
+    if(n == 1 or n == 0){
+        return 1;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+
 int main()
 {
-    cout << "Selamat pagi guys!";
+    // Program membuat suku ke-n bilangan dalam barisan fibonacci
+    int n;
+    cout << "PROGRAM BARISAN FIBONACCI" << endl;
+    cout << "Suku ke berapa yang ingin Anda cari?";
+    cin >> n;
+    cout << "Suku ke-" << n << " dalam barisan fibonacci adalah " << fibonacci(n);
     return 0;
 }
