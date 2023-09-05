92bbf9463a30ec27c2a37b903d224ec35481389b
ifwhy <ifanugrh02@student.uns.ac.id>
2023-09-05 09:15:54 +0700

    Menambahkan latihan.cpp

--- latihan.cpp
+++ latihan.cpp
@@ -3,17 +3,6 @@ using namespace std;
 
 int main()
 {
-    // Program membuat n suku pertama dalam barisan fibonacci
-    int n, fn, fn1 = 1, fn2 = 0;
-    cout << "PROGRAM MEMBUAT N SUKU PERTAMA DALAM BARISAN FIBONACI" << endl;
-    cout << "Masukkan sebuah bilangan asli : ";
-    cin >> n;
-
-    cout << n << " suku pertama dalam barisan fibonacci adalah : ";    for(int i = 1; i <= n; i++){
-        fn = fn1 + fn2;
-        cout << fn << " ";
-        fn2 = fn1;
-        fn1 = fn;
-    }
+    cout << "Hello, World!" << endl;
     return 0;
 }
