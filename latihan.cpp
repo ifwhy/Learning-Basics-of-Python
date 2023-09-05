b565a6b9584b71194fdee6f9e8a3f0a2d953d590
ifwhy <ifanugrh02@student.uns.ac.id>
2023-09-05 09:20:26 +0700

    Updated n barisan fibonacci latihan.cpp

--- latihan.cpp
+++ latihan.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, World!" << endl;
+    // Program membuat n suku pertama dalam barisan fibonacci
+    int n, fn, fn1 = 1, fn2 = 0;
+    cout << "PROGRAM MEMBUAT N SUKU PERTAMA DALAM BARISAN FIBONACI" << endl;
+    cout << "Masukkan sebuah bilangan asli : ";
+    cin >> n;
+
+    cout << n << " suku pertama dalam barisan fibonacci adalah : ";    
+    cout << "0 1 ";
+    for(int i = 1; i < n-1; i++){
+        fn = fn1 + fn2;
+        cout << fn << " ";
+        fn2 = fn1;
+        fn1 = fn;
+    }
     return 0;
 }
