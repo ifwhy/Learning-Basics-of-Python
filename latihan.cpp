2502465ae36f0bd356843f95e44a3b9afff9a180
ifwhy <ifanugrh02@student.uns.ac.id>
2023-09-29 09:35:24 +0700

    Menambahkan while TLX latihan.cpp

--- latihan.cpp
+++ latihan.cpp
@@ -3,17 +3,9 @@ using namespace std;
 
 int main()
 {
-    double n;
-    cin >> n;
-
-    while(n > 1){
-        n = n/2;
-
-    }
-    if(n == 1){
-        cout << "ya";
-    } else {
-        cout << "bukan";
+    char kalimat[101];
+    while((scanf("%s", kalimat)) != EOF){
+        cout << kalimat << endl;
     }
     return 0;
 }
