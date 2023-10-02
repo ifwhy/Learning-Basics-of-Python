72aadd6e931a921365f64424486a00dbe40bd193
ifwhy <ifanugrh02@student.uns.ac.id>
2023-10-02 21:34:56 +0700

    Menambahkan pola ketiga hackerrank TC main.py (UPDATED)

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 def ketupat(n):
+    k = n-1
     for i in range(1, n+2, 2):
-        k = n-1
-        if i == 1:
-            print(" "*(k) + "*"*i)
-        else :
-            print(" "*(k-i+1) + "* "*i)
+        print(" "*(k-i+1) + "* "*i)
     for j in range(n-2, 0, -2):
-        if j != 1:
-            print(" "*(k-j+1) + "* "*j)
-        else :
-            print(" "*k + "*"*j)
+        print(" "*(k-j+1) + "* "*j)
 
 x = int(input())
 
