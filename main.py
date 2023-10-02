e4efd3ef86514e9825666885bc27c77c75bf0586
ifwhy <ifanugrh02@student.uns.ac.id>
2023-10-02 22:16:25 +0700

    Menambahkan pola 8 hackerrank TC main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def ketupat(n):
     k = n-1
+    for j in range(n, 2, -2):
+        print(" "*(k-j+1) + "* "*j)
     for i in range(1, n+2, 2):
         print(" "*(k-i+1) + "* "*i)
-    for j in range(n-2, 0, -2):
-        print(" "*(k-j+1) + "* "*j)
 
 x = int(input())
 
