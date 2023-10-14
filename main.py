bf63e7f53ffb5f3ae819dcc1f984ff544c84f1ba
ifwhy <ifanugrh02@student.uns.ac.id>
2023-10-14 19:17:45 +0700

    Menambahkan program batu gunting kertas main.py

--- main.py
+++ main.py
@@ -1,28 +1,67 @@
-# Nama          : Dengklek bin ucup
-# NIM           : L0123(...)
-# Prodi/Kelas   : Sastra Pertanian/E
+import random as rd    
 
-def ketupat(n):
-    k = int((n+1)/2)
-    for i in range(1, k+1):
-        spasi = 4*i-7
-        bintang = int((2*n-1-spasi)/4)
-        if i == 1:
-            print("* "*(n))
-        else :  
-            print("* "*(bintang) + " "*(spasi) + " *"*(bintang))
-    for j in range(k-1, 0, -1): 
-        spasi = 4*j-7
-        bintang = int((2*n-1-spasi)/4)
-        if j == 1:
-            print("* "*(n))
-        else : 
-            print("* "*(bintang) + " "*(spasi) + " *"*(bintang))
-        
- 
-x = int(input())
- 
-if x%2 == 1:
-    ketupat(x)
-else :
-    ketupat(x-1)
+tanya = 1
+while (tanya == 1) : 
+    print("____PERMAINAN BATU GUNTING KERTAS DENGAN KOMPUTER____")
+    print("\t Masukkan angka 1 untuk BATU")
+    print("\t Masukkan angka 2 untuk GUNTING")
+    print("\t Masukkan angka 3 untuk KERTAS")
+    print("____________________________________________________")
+    n = int(input("Pilih di sini : "))
+
+    comp = rd.random()
+    if comp <= 0.3333 :
+        comp = 1
+    elif comp > 0.3333 and comp <= 0.666:
+        comp = 2
+    else :
+        comp = 3
+
+    if n == 1:
+        print("Anda memilih BATU")
+    elif n == 2:
+        print("Anda memilih GUNTING")
+    elif n == 3:
+        print("Anda memilih KERTAS")
+    else:
+        print("Masukan Anda salah")
+        break
+
+    if comp == 1:
+        print("Komputer memilih BATU")
+    elif comp == 2:
+        print("Komputer memilih GUNTING")
+    elif comp == 3:
+        print("Komputer memilih KERTAS")
+
+    print("Hasilnya adalah", end=" ")
+    if n == comp :
+        print("____SERI____")
+    elif n == 1:
+        if comp == 2:
+            print("____ANDA MENANG____")
+        elif comp == 3:
+            print("____ANDA KALAH____")
+    elif n == 2:
+        if comp == 1:
+            print("____ANDA KALAH____")
+        elif comp == 3:
+            print("____ANDA MENANG____")
+    elif n == 3:
+        if comp == 1:
+            print("____ANDA MENANG____")
+        elif comp == 2:
+            print("___ANDA KALAH____")
+
+    print()
+    tanya = str(input("Apakah Anda ingin bermain lagi? (y/n): "))
+    print()
+    if tanya == 'y':
+        tanya = 1
+    elif tanya == 'n':
+        tanya = 0
+    else:
+        print("Masukan Anda salah, program diakhiri")
+        break
+print("\t ____TERIMA KASIH SUDAH BERMAIN____")
+print("\t \t ____SAMPAI JUMPA____")
