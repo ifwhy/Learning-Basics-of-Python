25b383f0693b0c2b95d37a80eb7fc7c476688f00
ifwhy <ifanugrh02@student.uns.ac.id>
2023-11-03 17:00:22 +0700

    Menambahkan program konversi desimal ke biner latihan.cpp

--- latihan.cpp
+++ latihan.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int gcd(int a);
-int bil1, bil2, fpb = 0;
-int main()
-{
-    // Greatest common factor (fpb)
-    cout << "Masukkan bilangan pertama : ";
-    cin >> bil1;
-    cout << "Masukkan bilangan kedua : ";
-    cin >> bil2;
-
-    if(bil2 > bil1){
-        gcd(bil2);
-    } else {
-        gcd(bil1);
+void decToBinOkt(int n, int p){
+    int i = 0, data[n]; 
+    while(n > 0){
+        data[i] = n % p;
+        n /= p;
+        i++;
     }
-    printf("FPB-nya adalah %d", fpb);
-    return 0;
+    for(int j = i-1; j >= 0; j--){
+        cout << data[j] << " ";
+    }
 }
 
-int gcd(int a){
-    for(int i = 1; i <= a; i++){
-            if(bil1%i == 0 && bil2%i == 0){
-                if(i >= fpb){
-                    fpb = i;
-                }
-            }
-        }
-    return fpb;
+int main()
+{
+    int n;
+    cout << "Bilangan Desimal : ";
+    cin >> n;
+
+    cout << "Bilangan biner : ";
+    decToBinOkt(n, 2);
+    cout << endl << "Bilangan oktal : ";
+    decToBinOkt(n, 8);
+    return 0;
 }
