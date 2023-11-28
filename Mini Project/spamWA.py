9a09b6995fe1827ce45a1a1e2413911e68ff4d9b
Ivan Wahyu Nugroho <143915202+ifwhy@users.noreply.github.com>
2023-11-28 20:25:30 +0700

    Menambahkan projek Spammer WA

---
+++ Mini Project/spamWA.py
@@ -0,0 +1,14 @@
+import pyautogui as ag, pywhatkit as wk, time
+
+wk.sendwhatmsg("+6285876728762", "Bangun!!!!", 19, 13)
+ag.click()
+time.sleep(2)
+
+for i in range(10):
+    ag.press('i')
+    ag.press('v')
+    ag.press('a')
+    ag.press('n')
+    ag.press('🤣')
+    ag.press('enter')
+
